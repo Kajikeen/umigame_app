ed6eb8ebe485767f40d02e56d2b4fb11b64ce00c
梶原 <kajihara@kajiwaranoMacBook-Air.local>
2019-10-08 17:54:38 +0900

    191007_2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_06_054346) do
+ActiveRecord::Schema.define(version: 2019_10_08_081829) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_10_06_054346) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "answer"
   end
 
   create_table "users", force: :cascade do |t|
@@ -28,6 +29,7 @@ ActiveRecord::Schema.define(version: 2019_10_06_054346) do
     t.string "image_name"
     t.string "password_confirmation"
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
